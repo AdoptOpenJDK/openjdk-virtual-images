73d1190b2005830b66934dfea657b6064dc078db
openjdk <openjdk@java-pundit.(none)>
2013-02-11 13:27:18 +0000

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: buildOpenjdk
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "mercurial" do
+        action :install
+end
+
