d4cd1d39a0d186c171f5b01dfc9ac0e69386df72
=Kaushal Singh <=st.kaushal@gmail.com>
2013-02-19 11:13:35 +0000

    added Apache License

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,21 @@
+# Author : Kaushal Singh <home.ksingh@gmail.com>
+# Cookbook Name:: Openjdk
+# Recipe:: default
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 default[:openjdk][:dir] = "/home/vagrant/openjdk/"
-#default[:openjdk][:dir][:forest] = "/home/vagrant/opst/"
 default[:openjdk][:forest] = "#{node[:openjdk][:dir]}hgforest/"
 default[:openjdk][:source] = "#{node[:openjdk][:dir]}source/"
 default[:openjdk][:source_url] = "http://hg.openjdk.java.net/jdk8/tl"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,20 @@
+# Author : Kaushal Singh <home.ksingh@gmail.com>
 # Cookbook Name:: Openjdk
-# Recipe:: default
+# Recipe:: default	
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# All rights reserved - Do Not Redistribute
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-# this installing packge blog will and should go in loop... need to do, after POC
 package = %w{unzip zip libX11-dev libxext-dev libxrender-dev libxtst-dev libfreetype6-dev libcups2-dev libasound2-dev ccache g++-4.6-multilib}
 
 package.each do |pkg|
