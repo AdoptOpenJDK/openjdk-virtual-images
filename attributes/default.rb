3403d7b2155dab51d00959799fd87679ee54bc98
=Kaushal Singh <=st.kaushal@gmail.com>
2013-02-20 20:09:38 +0000

    added code to make openjdk image and configure jtreg

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,10 +14,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-default[:openjdk][:dir] = "/home/vagrant/openjdk/"
+# constants for building OpenJdk.
+default[:openjdk][:home] = "/home/vagrant/"
+default[:openjdk][:dir] = "#{node[:openjdk][:home]}openjdk/"
 default[:openjdk][:forest] = "#{node[:openjdk][:dir]}hgforest/"
 default[:openjdk][:source] = "#{node[:openjdk][:dir]}source/"
 default[:openjdk][:source_url] = "http://hg.openjdk.java.net/jdk8/tl"
 default[:openjdk][:forest_url] = "https://bitbucket.org/pmezard/hgforest-crew/overview/"
+default[:openjdk][:hgrc] = "#{node[:openjdk][:home]}.hgrc"
 default[:openjdk][:get_source] = "#{node[:openjdk][:source]}get_source.sh"
+
+#download and configre jtreg
+default[:openjdk][:jtreg][:dir] = "#{node[:openjdk][:dir]}jtreg/"
+default[:openjdk][:jtreg][:file] = "#{node[:openjdk][:jtreg][:dir]}jtreg-4.1.zip" 
+default[:openjdk][:jtreg][:url] = "http://www.java.net/download/openjdk/jtreg/promoted/4.1/b05/jtreg-4.1-src-b05_29_nov_2012.zip"
+default[:openjdk][:jtreg][:checksum] ="2ccacd2550f8094f0dcd1601748add3e"
+default[:openjdk][:product_home] = "#{node[:openjdk][:source]}build/linux-x64-normal-server-release/images/j2sdk-image/"
+default[:openjdk][:export_path] = "/etc/profile.d/openjdk_build_path.sh"
--- recipes/default.rb
+++ recipes/default.rb
@@ -15,6 +15,7 @@
 # limitations under the License.
 #
 
+# instaling dependancy packages
 package = %w{unzip zip libX11-dev libxext-dev libxrender-dev libxtst-dev libfreetype6-dev libcups2-dev libasound2-dev ccache g++-4.6-multilib}
 
 package.each do |pkg|
@@ -23,41 +24,68 @@ package.each do |pkg|
 	end
 end
 
+#openjdk_dirs = %w{#node[:openjdk][:dir] #node[:openjdk][:forest] #node[:openjdk][:source] #node[:openjdk][:jtreg][:dir]}
+
+#openjdk_dirs.each do |dir|
+#	directory dir do
+#	mode "0777"
+#	owner "root"
+#	action :create
+#	recursive true
+#	end
+#end
+
 directory node[:openjdk][:dir] do
 	owner "root"
 	mode "0755"
 	action :create
 end
 directory node[:openjdk][:forest] do
-	#owner "root"
+	owner "root"
 	mode "0755"
 	action :create
 end
+
 directory node[:openjdk][:source] do
-	owner "vagrant"
+	owner "root"
 	mode "0777"
 	action :create
 end
+
+directory node[:openjdk][:jtreg][:dir] do
+        owner "root"
+        mode "0777"
+        action :create
+end
+
+remote_file node[:openjdk][:jtreg][:file] do
+	source node[:openjdk][:jtreg][:url]
+	checksum node[:openjdk][:jtreg][:checksum]
+	mode "0777"
+	action :create_if_missing
+end
+
 
 mercurial node[:openjdk][:forest]  do
 	repository node[:openjdk][:forest_url]
 	mode "0755"
 	action :sync
 end
+
 mercurial node[:openjdk][:source] do 
 	repository node[:openjdk][:source_url]
 	mode "0755"
 	action :sync	
 end
 
-file "/home/vagrant/.hgrc" do 
+file node[:openjdk][:hgrc] do 
 	content <<-EOS
 forest = #{node[:openjdk][:forest]}forest.py
 	EOS
-	mode 0755
+	mode "0755"
 end
 
-execute "get source" do
+execute "get_source" do
 	user "root"        
 	cwd node[:openjdk][:source]
 	command "sh #{node[:openjdk][:get_source]}"
@@ -67,4 +95,24 @@ execute "auto_configure" do
 	user "root"
 	cwd node[:openjdk][:source]
 	command "bash configure"
+end
+
+execute "configure_jtreg" do
+	user "root"
+	cwd node[:openjdk][:jtreg][:dir]
+	command "unzip -u #{node[:openjdk][:jtreg][:file]}"
+end
+
+execute "build_openjdk_images" do 
+	user "root"
+	cwd node[:openjdk][:source]
+	command "make clean images"
+end
+
+file node[:openjdk][:export_path] do
+  content <<-EOS
+    export JT_HOME=#{node[:openjdk][:jtreg][:dir]}
+    export PRODUCT_HOME=#{node[:openjdk][:product_home]}
+  EOS
+  mode 0755
 end
