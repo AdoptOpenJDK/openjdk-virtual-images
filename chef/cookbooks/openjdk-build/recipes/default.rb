d6e61b8b97b0bb13b6807ce3f827546f0d0abdd3
Martijn Verburg <martijnverburg@gmail.com>
2014-02-06 17:42:56 +0000

    Add longer timeut for get_source script

--- chef/cookbooks/openjdk-build/recipes/default.rb
+++ chef/cookbooks/openjdk-build/recipes/default.rb
@@ -24,7 +24,6 @@ package.each do |pkg|
 	end
 end 
 
-
 user "openjdk" do
  comment "Openjdk User"
  uid 1005
@@ -43,7 +42,6 @@ group "openjdk" do
   action :create
   append true
 end
-
 
 remote_file node[:jtreg][:file] do
   source node[:jtreg][:url]
@@ -123,6 +121,7 @@ node[:openjdk].each do |index, jdk|
             user node[:user]
             cwd jdk[:source_tl]
             command "sh #{jdk[:get_source]}"
+            timeout 7200
     end
 
 
@@ -131,7 +130,6 @@ node[:openjdk].each do |index, jdk|
             cwd jdk[:source_tl]
             command "bash configure #{bootJdkArgs}"
     end
-
 
     execute "build_openjdk_clean_images" do 
             user node[:user]
@@ -152,7 +150,6 @@ node[:openjdk].each do |index, jdk|
     	timeout 72000
     	only_if { ::File.exist?("#{jdk[:build_log_file]}") } # only if the build.log file exists
     end
-
 
     ruby_block "include-bashrc" do
       block do
